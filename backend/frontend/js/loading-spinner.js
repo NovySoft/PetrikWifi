aa0f74128f27861d291997c69b75ef5d16ed5ad4
Legolaszstudio <novotny.levente@gmail.com>
2025-01-23 18:56:41 +0100

    Loaders take one

---
+++ backend/frontend/js/loading-spinner.js
@@ -0,0 +1,7 @@
+function showLoading() {
+    document.getElementById("loader_overlay").classList.remove('hide');
+}
+
+function hideLoading() {
+    document.getElementById("loader_overlay").classList.add('hide');
+}
