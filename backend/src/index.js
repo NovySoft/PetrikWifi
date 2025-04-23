45efde8c1a5e7d9235fd9bdd9d63dbe6259460eb
Legolaszstudio <novotny.levente@gmail.com>
2025-04-23 15:54:30 +0200

    Add nextcloud backup mechanism

--- backend/src/index.js
+++ backend/src/index.js
@@ -262,9 +262,13 @@ fastify.register(fastifyStatic, {
 
 import { Cron } from 'croner';
 import { cleanDatabase } from './databaseCleaner.js';
+import { createClient } from "webdav";
+if (process.env.NX_CLOUD_URL == null || process.env.NX_CLOUD_USER == null || process.env.NX_CLOUD_PASS == null) {
+    logger.warn("NextCloud credentials not set. Database and log will not be backed up!");
+}
 // Every minute: '* * * * *'
 // Every day at 2:00: '0 2 * * *'
-const job = new Cron('0 2 * * *', async () => {
+const job = new Cron('* * * * *', async () => {
     logger.info('Database cleaning started.');
     let time = Date.now();
     await cleanDatabase();
@@ -276,8 +280,54 @@ const job = new Cron('0 2 * * *', async () => {
     const pad = num => (num > 9 ? "" : "0") + num;
     const backupFileName = `./database/backup-${backupDate.getFullYear()}-${pad(backupDate.getMonth() + 1)}-${pad(backupDate.getDate())}.db`
     db.backup(backupFileName)
-        .then(() => {
+        .then(async () => {
             logger.info(`Database backup finished. (${backupFileName}) Took ${Date.now() - backupTime}ms.`);
+
+            if (process.env.NX_CLOUD_URL == null || process.env.NX_CLOUD_USER == null || process.env.NX_CLOUD_PASS == null) {
+                logger.warn("NextCloud credentials not set. Database and log will not be backed up!");
+                return;
+            }
+
+            try {
+                const client = createClient(
+                    process.env.NX_CLOUD_URL + '/remote.php/dav/files/' + process.env.NX_CLOUD_USER,
+                    {
+                        username: process.env.NX_CLOUD_USER,
+                        password: process.env.NX_CLOUD_PASS,
+                    }
+                );
+                const directoryItems = await client.getDirectoryContents("/");
+
+                if (!directoryItems.some(item => item.filename === '/Databases')) {
+                    logger.info('Creating /Databases directory on NextCloud...');
+                    await client.createDirectory('/Databases');
+                }
+                if (!directoryItems.some(item => item.filename === '/Logs')) {
+                    logger.info('Creating /Logs directory on NextCloud...');
+                    await client.createDirectory('/Logs');
+                }
+
+                let time = Date.now();
+                logger.info('Uploading database backup to NextCloud...');
+                await client.putFileContents('/Databases/' + backupFileName.split('/').pop(), fs.createReadStream(backupFileName), { overwrite: true });
+                logger.info('Today\'s database backup uploaded to NextCloud. Took ' + (Date.now() - time) + 'ms.');
+
+                time = Date.now();
+                logger.info('Uploading log file to NextCloud...');
+                const date = new Date(time);
+                // Subtract 1 day to get the correct date for the log file name (saving yesterday's logs to yesterday's file)
+                date.setDate(date.getDate() - 1);
+                const logFilePath = `./logs/${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}.log`;
+                if (fs.existsSync(logFilePath)) {
+                    await client.putFileContents('/Logs/' + logFilePath.split('/').pop(), fs.createReadStream(logFilePath), { overwrite: true });
+                    logger.info('Yesterday\'s log file uploaded to NextCloud. Took ' + (Date.now() - time) + 'ms.');
+                } else {
+                    logger.warn(`Yesterday's log file not found. (${logFilePath}) Skipping upload.`);
+                }
+            } catch (err) {
+                logger.error('Error while uploading database backup to NextCloud: ' + err);
+                Sentry.captureException(err);
+            }
         })
         .catch((err) => {
             logger.error('Database backup failed: ' + err);
